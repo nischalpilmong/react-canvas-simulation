f2b26153b7159c06dd728ac94113f382fef20a5d
Sandeep Ghimire <s.ghimire@laserhub.de>
2019-06-30 09:30:10 +0200

    code filter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
-import Canvas from '../src/components/Canvas/Canvas'
-import CanvasPaths from '../src/components/CanvasPaths/CanvasPaths'
-import CanvasClick from './components/UserMovementSimulation/CanvasClick'
-import MouseCordinates from '../src/components/MouseCoordinates/MouseCoordinates'
-import Animation from '../src/components/Animation/Animation'
-import CanvasClicks from './components/ProblemSimulation/CanvasClick'
-import Home from '../src/components/Home/Home'
 import Dashboard from '../src/components/Dashboard'
 import { BrowserRouter } from 'react-router-dom';
 
-
-import classes from './App.css';
 class App extends Component {
   render() {
     return (
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import { Route, Link } from 'react-router-dom';
 import CanvasClick from './UserMovementSimulation/CanvasClick';
 import CanvasClicks from './ProblemSimulation/CanvasClick';
